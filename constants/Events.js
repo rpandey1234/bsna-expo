0a80e92877b86ddc5befce3cf0a74475bfbeed41
Rahul Pandey <rahul@pinterest.com>
2017-07-09 23:52:42 -0700

    Delete past event

--- constants/Events.js
+++ constants/Events.js
@@ -1,10 +1,5 @@
 export default {
     events: [
-        { "title": "Michigan chapter picnic",
-            "location": "Rochester, MI",
-            "date": "July 8, 2017",
-            "link": "http://evite.me/6Q4fZmEbW3"
-        },
         { "title": "22nd Annual BSNA Convention",
             "location": "Auburn Hills, MI",
             "date": "July 28 - 30, 2017"
