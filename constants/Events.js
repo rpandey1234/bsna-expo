97284192075d9133fcade9cd953997dabd8e826d
Rahul Pandey <rahul@pinterest.com>
2017-07-16 11:43:41 -0700

    Clean up home screen, add additional description for convention

--- constants/Events.js
+++ constants/Events.js
@@ -2,7 +2,9 @@ export default {
     events: [
         { "title": "22nd Annual BSNA Convention",
             "location": "Auburn Hills, MI",
-            "date": "July 28 - 30, 2017"
+            "date": "July 28 - 30, 2017",
+            "details": "The 22nd Annual BSNA Convention will be held at the Auburn Hills Marriott in Michigan from July 28 - 30, 2017. The cost for 2 people with hotel accommodation is $499. The theme this year is 'Role of Women in Shaping Brahamic Values.'",
+            "link": "http://www.bsna.org/Default.aspx",
         }
     ]
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Button,
   Dimensions,
   Image,
   Linking,
@@ -7,30 +8,19 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import Events from '../constants/Events.js';
 import { MonoText } from '../components/StyledText';
 
-export class Card extends React.Component {
-  render() {
-    return (
-        <View style={styles.card}>
-          <Text>{this.props.title}</Text>
-          <Text>{this.props.location}</Text>
-          <Text>{this.props.date}</Text>
-          {this.props.link ? <Text style={styles.helpLinkText} onPress={() => Linking.openURL(this.props.link)}>More info</Text> : undefined }
-        </View>
-    )
-  }
-}
-
 export class Loading extends React.Component {
   render() {
-    return (<View style={styles.centerContent}>
+    return <View>
       {this.props.loading ? <Image source={require('../assets/images/loading.gif')} style={styles.loadingImage}/> :
           undefined}
-    </View>);
+    </View>;
   }
 }
 
@@ -70,58 +60,38 @@ export default class HomeScreen extends React.Component {
           </View>
 
           <View style={styles.getStartedContainer}>
-
             <Text style={styles.getStartedText}>
               Brahman Samaj of North America (BSNA) is the largest Brahman organization with the sole objective of
               bringing Brahmans together and helping them seek cultural and social identity.
             </Text>
           </View>
 
-          <Text style={[styles.getStartedText, styles.bold]}>
-            Upcoming Events
-          </Text>
           {this.state.events.map(function(event, index) {
-            return <Card key={index} title={event.title} location={event.location} date={event.date} link={event.link}/>
+            return <View key={index} style={styles.card}>
+              <Text style={styles.bold}>{event.title}</Text>
+              <Text>{event.location}</Text>
+              <Text>{event.date}</Text>
+              {event.link ? <Text style={styles.helpLinkText} onPress={() => Linking.openURL(event.link)}>More info</Text> : undefined }
+              <Text style={styles.details}>{event.details}</Text>
+            </View>;
           })}
           <View
               style={{
                 borderBottomWidth: 1,
                 borderBottomColor: 'lightgray',
                 width: Dimensions.get('window').width,
-              }}/>
+                marginTop: 10
+              }}
               />
         </ScrollView>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={() => Linking.openURL('https://docs.expo.io/versions/latest/guides/development-mode')} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will run slightly slower but
-          you have access to useful development tools. {learnMoreButton}.
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
 }
 
 const styles = StyleSheet.create({
   card: {
-    backgroundColor: 'skyblue',
+    backgroundColor: 'oldlace',
     marginBottom: 10,
     marginTop: 10,
     paddingBottom: 10,
@@ -147,12 +117,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 15,
-    textAlign: 'center',
-  },
   contentContainer: {
     paddingTop: 20,
   },
@@ -166,10 +130,6 @@ const styles = StyleSheet.create({
     height: 60,
     resizeMode: 'contain',
     marginTop: 3,
-    marginLeft: -10,
-  },
-  centerContent: {
-    alignItems: 'center',
   },
   loadingImage: {
     resizeMode: 'contain',
@@ -178,17 +138,6 @@ const styles = StyleSheet.create({
   getStartedContainer: {
     alignItems: 'center',
     marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
   },
   getStartedText: {
     fontSize: 17,
@@ -200,43 +149,11 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: 'bold',
   },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
   helpLinkText: {
-    fontSize: 14,
+    fontSize: 18,
     color: '#2e78b7',
+  },
+  details: {
+    marginTop: 15,
   },
 });
