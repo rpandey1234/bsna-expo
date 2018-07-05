b94a5463bdaf71a0fe01a734c5f1c44ea109d05f
Rahul Pandey <rpandey1234@gmail.com>
2018-07-04 18:58:28 -0700

    Upgrade SDK and add 2018 BSNA convention event

--- constants/Events.js
+++ constants/Events.js
@@ -1,5 +1,11 @@
 export default {
     events: [
+        { "title": "23rd Annual BSNA Convention",
+            "location": "Houston, TX",
+            "date": "July 27 – July 29, 2018",
+            "details": "The 23rd Annual BSNA Convention will be held at the Doubletree by Hilton in Houston, Texas. The cost for 2 people with hotel accommodation is $499. The convention director is Mrs. Abha Dwivedi.",
+            "link": "http://www.bsna.org/Default.aspx",
+        },
         { "title": "22nd Annual BSNA Convention",
             "location": "Auburn Hills, MI",
             "date": "July 28 - 30, 2017",
